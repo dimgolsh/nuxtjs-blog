5df7848b9ff794f976e479b68fb9eeb26831a9f3
dimon <dimagol64@gmail.com>
2020-04-27 20:19:19 +0300

    post controller and routes

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,6 +1,46 @@
-const passport = require('passport')
+const passport = require("passport");
+const { Router } = require("express");
+const router = new Router();
+const ctr = require("../controllers/post.controller");
 
-const {Router} = require('express')
-const router = new Router()
+//Admin
+// /api/post/admin
 
-module.exports =  router
+router.post(
+  "/admin/",
+  passport.authenticate("jwt", { session: false }),
+  ctr.create
+);
+
+router.get(
+  "/admin/",
+  passport.authenticate("jwt", { session: false }),
+  ctr.getAll
+);
+
+router.get(
+  "/admin/:id",
+  passport.authenticate("jwt", { session: false }),
+  ctr.getById
+);
+
+router.put(
+  "/admin/:id",
+  passport.authenticate("jwt", { session: false }),
+  ctr.update
+);
+
+router.delete(
+  "/admin/:id",
+  passport.authenticate("jwt", { session: false }),
+  ctr.remove
+);
+
+//base
+//api/post
+router.get("/", ctr.getAll);
+
+router.get("/:id", ctr.getById);
+router.put("/:id", ctr.addView);
+
+module.exports = router;
