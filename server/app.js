750a07c5a03760d71f21d7d72d6db90918c06e51
dimon <dimagol64@gmail.com>
2020-04-27 20:11:33 +0300

    passport

--- server/app.js
+++ server/app.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const authRoutes = require('./routes/auth.routes')
-const keys = require('./keys')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("password");
+const authRoutes = require("./routes/auth.routes");
+const postRoutes = require("./routes/post.routes");
+const keys = require("./keys");
+const passportStrategy = require("./middleware/passport-strategy");
+const app = express();
 
-mongoose.connect(keys.MONGO_URI)
-  .then(() => console.log('MongoDB connected...'))
-  .catch(error => console.error(error))
+mongoose
+  .connect(keys.MONGO_URI)
+  .then(() => console.log("MongoDB connected..."))
+  .catch(error => console.error(error));
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(passport.initialize());
+passport.use(passportStrategy);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
-app.use('/api/auth', authRoutes)
-
-module.exports = app 
+module.exports = app;
